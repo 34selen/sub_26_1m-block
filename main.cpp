56addd47189466b1b7057df4d10d04028b5b5eda
34selen <73600142+34selen@users.noreply.github.com>
2025-05-22 00:13:48 +0900

    ff

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
-#include <errno.h>
+#include <cerrno>
 #include <linux/types.h>
 
 #include <netinet/in.h>
-#include <linux/netfilter.h> /* for NF_ACCEPT */
+#include <linux/netfilter.h>
 #include <libnet.h>
 #include <libnetfilter_queue/libnetfilter_queue.h>
 
@@ -14,185 +14,156 @@
 #include <unordered_set>
 #include <iostream>
 #include <fstream>
-#include <iso646.h>
 
-std::string method[9] = {"GET", "POST", "HEAD", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"};
-std::unordered_set<std::string> block;
+std::string http_methods[] = {"GET", "POST", "HEAD", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"};
+std::unordered_set<std::string> block_list;
 
-std::string host_from_packet(const char *packet, unsigned int len)
+std::string extract_host(const char *data, unsigned int len)
 {
-    int index = 0;
-    while ((index < len) and (strncmp("Host:", packet, 5)))
+    for (unsigned int i = 0; i + 5 < len; ++i)
     {
-        index++;
-        packet++;
+        if (strncmp(data + i, "Host:", 5) == 0)
+        {
+            const char *ptr = data + i + 5;
+            if (*ptr == ' ')
+                ++ptr;
+            std::string host;
+            while (*ptr != '\r' && (ptr - data) < (int)len)
+            {
+                host += *ptr++;
+            }
+            return host;
+        }
     }
-    if (index == len)
-        return NULL;
-
-    packet += 5;
-    if (*packet == ' ')
-        packet++;
-
-    std::string tmp;
-    while (*packet not_eq '\r')
-        tmp += *packet++;
-    return tmp;
+    return "";
 }
 
-static bool check_host(unsigned char *packet, unsigned int len)
+bool should_block(unsigned char *pkt, unsigned int len)
 {
-    struct libnet_ipv4_hdr *ipv4 = (struct libnet_ipv4_hdr *)packet;
-
-    if (ipv4->ip_p not_eq IPPROTO_TCP)
-        return false;
-    struct libnet_tcp_hdr *TCP = (struct libnet_tcp_hdr *)(packet + ipv4->ip_hl * 4);
-    const char *payload = (const char *)(packet + ipv4->ip_hl * 4 + TCP->th_off * 4);
-
-    if ((ntohs(TCP->th_sport) not_eq 80) and (ntohs(TCP->th_dport) not_eq 80))
+    auto *ip = (struct libnet_ipv4_hdr *)pkt;
+    if (ip->ip_p != IPPROTO_TCP)
         return false;
 
-    bool IsHTTP = false;
-    for (std::string str : method)
+    auto *tcp = (struct libnet_tcp_hdr *)(pkt + ip->ip_hl * 4);
+    const char *payload = (const char *)(pkt + ip->ip_hl * 4 + tcp->th_off * 4);
+
+    uint16_t src_port = ntohs(tcp->th_sport);
+    uint16_t dst_port = ntohs(tcp->th_dport);
+    if (src_port != 80 && dst_port != 80)
+        return false;
+
+    bool is_http = false;
+    for (const auto &m : http_methods)
     {
-        if (IsHTTP)
+        if (strncmp(payload, m.c_str(), m.size()) == 0)
+        {
+            is_http = true;
             break;
-        IsHTTP |= (not strncmp(str.c_str(), payload, str.size()));
+        }
     }
-    if (not IsHTTP)
+    if (!is_http)
         return false;
 
-    std::string host = host_from_packet(payload, len);
-    auto iter = block.find(host);
-    if (iter == block.end())
-        return false;
+    std::string host = extract_host(payload, len);
+    if (block_list.count(host))
+    {
+        printf("[+] Blocked domain: %s\n", host.c_str());
+        return true;
+    }
 
-    printf("BLOCK!! : %s\n", host.c_str());
-    return true;
+    return false;
 }
 
-static unsigned int get_id(struct nfq_data *tb)
+uint32_t fetch_packet_id(struct nfq_data *nfa)
 {
-    struct nfqnl_msg_packet_hdr *ph = nfq_get_msg_packet_hdr(tb);
-    int id = 0;
-    if (ph)
-        id = ntohl(ph->packet_id);
-    return id;
+    struct nfqnl_msg_packet_hdr *hdr = nfq_get_msg_packet_hdr(nfa);
+    if (!hdr)
+        return 0;
+    return ntohl(hdr->packet_id);
 }
 
-static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
-              struct nfq_data *nfa, void *data)
+int packet_handler(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+                   struct nfq_data *nfa, void *data)
 {
-    unsigned char *packet;
-    unsigned int id = get_id(nfa);
-    unsigned int len = nfq_get_payload(nfa, &packet);
+    unsigned char *buf = nullptr;
+    uint32_t pkt_id = fetch_packet_id(nfa);
+    int pkt_len = nfq_get_payload(nfa, &buf);
 
-    if (check_host(packet, len))
-        return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+    if (should_block(buf, pkt_len))
+        return nfq_set_verdict(qh, pkt_id, NF_DROP, 0, nullptr);
     else
-        return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+        return nfq_set_verdict(qh, pkt_id, NF_ACCEPT, 0, nullptr);
 }
 
-void MAKE_HASH(const char *file)
+void load_blocklist(const char *filename)
 {
-    std::ifstream input(file);
+    std::ifstream infile(filename);
     std::string line;
-    while (std::getline(input, line))
+    while (std::getline(infile, line))
     {
         if (line.empty())
             continue;
-        size_t comma = line.find(',');
-        if (comma == std::string::npos)
+        size_t pos = line.find(',');
+        if (pos == std::string::npos)
             continue;
-        std::string domain = line.substr(comma + 1);
-        block.insert(domain);
+        std::string domain = line.substr(pos + 1);
+        block_list.insert(domain);
     }
 }
 
-// main은 코드 그대로
 int main(int argc, char **argv)
 {
-    if (argc not_eq 2)
+    if (argc != 2)
     {
-        printf("syntax : 1m-block <site list file>\n");
-        printf("sample : 1m-block top-1m.txt\n");
-        return -1;
+        fprintf(stderr, "Usage: %s <csv_file>\n", argv[0]);
+        return 1;
     }
-    MAKE_HASH(argv[1]);
 
-    struct nfq_handle *h;
-    struct nfq_q_handle *qh;
-    struct nfnl_handle *nh;
-    int fd;
-    int rv;
+    load_blocklist(argv[1]);
+
+    struct nfq_handle *handle = nfq_open();
+    if (!handle)
+    {
+        perror("nfq_open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (nfq_unbind_pf(handle, AF_INET) < 0)
+    {
+        perror("nfq_unbind_pf");
+        exit(EXIT_FAILURE);
+    }
+
+    if (nfq_bind_pf(handle, AF_INET) < 0)
+    {
+        perror("nfq_bind_pf");
+        exit(EXIT_FAILURE);
+    }
+
+    struct nfq_q_handle *queue = nfq_create_queue(handle, 0, &packet_handler, nullptr);
+    if (!queue)
+    {
+        perror("nfq_create_queue");
+        exit(EXIT_FAILURE);
+    }
+
+    if (nfq_set_mode(queue, NFQNL_COPY_PACKET, 0xffff) < 0)
+    {
+        perror("nfq_set_mode");
+        exit(EXIT_FAILURE);
+    }
+
+    int fd = nfq_fd(handle);
     char buf[4096] __attribute__((aligned));
+    int received;
 
-    printf("opening library handle\n");
-    h = nfq_open();
-    if (!h)
+    while ((received = recv(fd, buf, sizeof(buf), 0)) && received >= 0)
     {
-        fprintf(stderr, "error during nfq_open()\n");
-        exit(1);
+        nfq_handle_packet(handle, buf, received);
     }
 
-    printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
-    if (nfq_unbind_pf(h, AF_INET) < 0)
-    {
-        fprintf(stderr, "error during nfq_unbind_pf()\n");
-        exit(1);
-    }
+    nfq_destroy_queue(queue);
+    nfq_close(handle);
 
-    printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
-    if (nfq_bind_pf(h, AF_INET) < 0)
-    {
-        fprintf(stderr, "error during nfq_bind_pf()\n");
-        exit(1);
-    }
-
-    printf("binding this socket to queue '0'\n");
-    qh = nfq_create_queue(h, 0, &cb, NULL);
-    if (!qh)
-    {
-        fprintf(stderr, "error during nfq_create_queue()\n");
-        exit(1);
-    }
-
-    printf("setting copy_packet mode\n");
-    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0)
-    {
-        fprintf(stderr, "can't set packet_copy mode\n");
-        exit(1);
-    }
-
-    fd = nfq_fd(h);
-
-    for (;;)
-    {
-        if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0)
-        {
-            nfq_handle_packet(h, buf, rv);
-            continue;
-        }
-
-        if (rv < 0 && errno == ENOBUFS)
-        {
-            printf("losing packets!\n");
-            continue;
-        }
-        perror("recv failed");
-        break;
-    }
-
-    printf("unbinding from queue 0\n");
-    nfq_destroy_queue(qh);
-
-#ifdef INSANE
-    printf("unbinding from AF_INET\n");
-    nfq_unbind_pf(h, AF_INET);
-#endif
-
-    printf("closing library handle\n");
-    nfq_close(h);
-
-    exit(0);
+    return 0;
 }
